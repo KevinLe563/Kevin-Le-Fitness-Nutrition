a004c80b48f9012b77c9e0439251178bbddac578
Kevin <k26le@uwaterloo.ca>
2020-05-30 02:57:41 -0600

    tion () {     $('input[type="checkbox"]').click(function () {       if ($("#rows").is(":checked")) {         $("#v-rows").css("visibility", "visible");       } else {         $("#v-rows").css("visibility", "hidden");       }     });   });

--- personal-fitness-plan.js
+++ personal-fitness-plan.js
@@ -1,49 +1,7 @@
-$(document).ready(function () {
-  $('input[type="checkbox"]').click(function () {
-    if ($("#barbell-shoulder-press").is(":checked")) {
-      $("#v-barbell-shoulder-press").css("visibility", "visible");
-    } else {
-      $("#v-barbell-shoulder-press").css("visibility", "hidden");
-    }
-  });
-});
-
-$(document).ready(function () {
-  $('input[type="checkbox"]').click(function () {
-    if ($("#bench-press").is(":checked")) {
-      $("#v-bench-press").css("visibility", "visible");
-    } else {
-      $("#v-bench-press").css("visibility", "hidden");
-    }
-  });
-});
-
-$(document).ready(function () {
-  $('input[type="checkbox"]').click(function () {
-    if ($("#rows").is(":checked")) {
-      $("#v-rows").css("visibility", "visible");
-    } else {
-      $("#v-rows").css("visibility", "hidden");
-    }
-  });
-});
-
-$(document).ready(function () {
-  $('input[type="checkbox"]').click(function () {
-    if ($("#deadlift").is(":checked")) {
-      $("#v-deadlift").css("visibility", "visible");
-    } else {
-      $("#v-deadlift").css("visibility", "hidden");
-    }
-  });
-});
-
-$(document).ready(function () {
-  $('input[type="checkbox"]').click(function () {
-    if ($("#barbell-curl").is(":checked")) {
-      $("#v-barbell-curl").css("visibility", "visible");
-    } else {
-      $("#v-barbell-curl").css("visibility", "hidden");
-    }
-  });
+$(document).on("click", ".exercise", function () {
+  if (this.checked) {
+    $(this).next().next().css("visibility", "visible");
+  } else {
+    $(this).next().next().css("visibility", "hidden");
+  }
 });
